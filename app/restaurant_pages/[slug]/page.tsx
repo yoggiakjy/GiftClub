1247d5fda412d1b0ccaafb3a8a8af6ad156105e5
Matthew <matthewyw@student.unimelb.edu.au>
2025-04-24 21:24:12 +1000

    merged but not implemented link

---
+++ app/restaurant_pages/[slug]/page.tsx
@@ -0,0 +1,16 @@
+// /app/restaurant_pages/[slug]/page.tsx
+import RestaurantTemplate from '../RestaurantTemplate';
+import { getPageBySlug } from '../data/pages';
+import { notFound } from 'next/navigation';
+
+type Props = {
+  params: { slug: string };
+};
+
+export default function DynamicPage({ params }: Props) {
+  const page = getPageBySlug(params.slug);
+
+  if (!page) return notFound();
+
+  return <RestaurantTemplate title={page.title} image = {page.image} discount ="" description='' opening_hours='' voucher_time='' address='' />;
+}
---
+++ app/restaurant_pages/data/pages.ts
@@ -0,0 +1,12 @@
+// /app/restaurant_pages/data/pages.tsx
+export const pages = [
+  { slug: 'page1', title: 'Page One', image: '/images/pizza.png' },
+  { slug: 'page2', title: 'Page Two', image: '/images/image2.jpg' },
+  { slug: 'page3', title: 'Page Three', image: '/images/image3.jpg' },
+  { slug: 'page4', title: 'Page Four', image: '/images/image4.jpg' },
+  { slug: 'page5', title: 'Page Five', image: '/images/image5.jpg' },
+];
+
+export function getPageBySlug(slug: string) {
+  return pages.find((page) => page.slug === slug);
+}
