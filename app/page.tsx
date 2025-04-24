ce5f6c694f4401e1aadfb717e4cc3291babb6c30
Antony Kim <antonykim7@gmail.com>
2025-04-24 22:14:12 +1000

    Hero section for landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Navbar from "./components/Navbar";
 import { NavbarLinks } from "./lib/data";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
@@ -8,14 +9,7 @@ export default function Home() {
       <Navbar navbarItems={NavbarLinks} />
 
       {/* Hero */}
-      <p className="text-6xl w-[35rem]">Eat out without forking out!</p>
-      <p className="text-4xl font-light w-[50rem]">
-        Save up to 50% off the total bill at the best restaurants and bars in
-        your city
-      </p>
-      <Link href={"/explore"} className="text-white">
-        <p>click here for explor epage</p>
-      </Link>
+      <Hero />
     </div>
   );
 }
