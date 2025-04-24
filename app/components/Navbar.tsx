6373947945264979b5844a6eabaf6fbc7e2729f0
Antony Kim <antonykim7@gmail.com>
2025-04-24 15:52:29 +1000

    Created navbar component and adjusted home landing page.

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface NavbarProps {
+    title: string;
+}
+
+const Navbar = ({
+    navbarItems
+} : {
+    navbarItems: NavbarProps[];
+}) => {
+  return (
+    <div className=''>
+
+    </div>
+  )
+}
+
+export default Navbar
